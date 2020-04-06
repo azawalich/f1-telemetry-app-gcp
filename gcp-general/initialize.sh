93699c2f04c960abd897e3d220a1cd62398a5184
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-04-06 22:55:24 +0200

    adding project initialization code

---
+++ gcp-general/initialize.sh
@@ -0,0 +1,15 @@
+### 0. INSTALLATION ###
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+### 1. INITIALIZATION ### 
+gcloud init
+
+# create dedicated project
+gcloud projects create f1-telemetry-app --set-as-default
