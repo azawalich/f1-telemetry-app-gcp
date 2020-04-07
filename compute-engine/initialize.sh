343504f8b3605209c6c29cf8fd8c6af80063ec9d
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-04-07 23:27:17 +0200

    fixes in compute engine initialize.sh

--- compute-engine/initialize.sh
+++ compute-engine/initialize.sh
@@ -1,5 +1,9 @@
 gcloud beta compute --project=f1-telemetry-app instances create f1-telemetry-app-vm --zone=europe-west3-a --machine-type=f1-micro --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=237255567678-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=ubuntu-1910-eoan-v20200331 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=f1-telemetry-app-vm --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 
+#reserve static IP
+gcloud compute addresses create vm-static-ip --project=f1-telemetry-app --network-tier=STANDARD --region=europe-west3
+gcloud compute instances add-access-config f1-telemetry-app-vm --project=f1-telemetry-app --zone=europe-west3-a --address=IP_OF_THE_NEWLY_CREATED_STATIC_ADDRESS --network-tier=STANDARD
+
 gcloud beta compute ssh --zone "europe-west3-a" "f1-telemetry-app-vm" --project "f1-telemetry-app"
 
 #making gcr.io images work within a VM
@@ -18,4 +22,11 @@ exit
 
 #after login 
 docker pull gcr.io/f1-telemetry-app/f1-telemetry-app-gcp
+
+tmux
+
 docker run -it -p 0.0.0.0:5005:5005 -p 0.0.0.0:5005:5005/udp gcr.io/f1-telemetry-app/f1-telemetry-app-gcp
+
+#click ctrl+b and d
+
+logout
