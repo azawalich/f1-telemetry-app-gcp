d7e78489cfb7ec0aa02c683b46171b1511bb5bcb
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-05-05 20:23:03 +0200

    switch from Dataflow to locally running Apache Beam

--- container-registry/dataflow/launch.py
+++ container-registry/dataflow/launch.py
@@ -123,21 +123,31 @@ def conv_token_auth_handler_on():
         return Response(error_msg, 403)
     else:
         # Dataflow accepts only names consisting of [a-z0-9-]
-        job_name = re.sub('_|:', '-', 'gcs_raw_to_gcs_clean_{}'.format(raw_folder))
+        # job_name = re.sub('_|:', '-', 'gcs_raw_to_clean_and_bigquery_{}'.format(raw_folder))
         if len(raw_folder) > 0:
+            # open_dataflow_cmd = """
+            # python3 gcs_raw_to_clean_and_bigquery.py \
+            # --job_name={} \
+            # --project={} \
+            # --gcs_input_files=gs://{}/raw/{}/-* \
+            # --output_path=gs://{}/clean/{}/ \
+            # --runner=DataflowRunner \
+            # --region={} \
+            # --temp_location=gs://{}/clean/{}/temp \
+            # --requirements_file requirements.txt \
+            # --experiments=allow_non_updatable_job
+            # """.format(
+            #     job_name, secrets['project_name'], secrets['bucket_name'], 
+            #     raw_folder, secrets['bucket_name'], raw_folder, secrets['region'], 
+            #     secrets['bucket_name'], raw_folder)
             open_dataflow_cmd = """
-            python3 gcs_raw_to_gcs_clean.py \
-            --job_name={} \
-            --project={} \
+            python3 gcs_raw_to_clean_and_bigquery.py \
             --gcs_input_files=gs://{}/raw/{}/-* \
             --output_path=gs://{}/clean/{}/ \
-            --runner=DataflowRunner \
-            --region={} \
-            --temp_location=gs://{}/clean/{}/temp \
-            --requirements_file requirements.txt
+            --temp_location=gs://{}/clean/{}/temp
             """.format(
-                job_name, secrets['project_name'], secrets['bucket_name'], 
-                raw_folder, secrets['bucket_name'], raw_folder, secrets['region'], 
+                secrets['bucket_name'], 
+                raw_folder, secrets['bucket_name'], raw_folder, 
                 secrets['bucket_name'], raw_folder)
             
             subprocess.Popen(open_dataflow_cmd, shell=True)
