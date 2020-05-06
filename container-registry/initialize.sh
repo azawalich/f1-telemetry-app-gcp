def3835b1458f184fee49ff0bafb9d4e218167eb
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-05-06 17:15:06 +0200

    dataflow and pubsub in main app folder - reverting back to old structure

--- container-registry/initialize.sh
+++ container-registry/initialize.sh
@@ -1,10 +1,10 @@
 cd ../../f1-telemetry-app-gcp
 gcloud auth configure-docker
 
-docker build --file ./container-registry/dataflow/Dockerfile -t dataflow . --no-cache
+docker build --file ./dataflow/Dockerfile -t dataflow . --no-cache
 docker tag dataflow gcr.io/f1-telemetry-app/dataflow
 docker push gcr.io/f1-telemetry-app/dataflow
 
-docker build --file ./container-registry/pub-sub/Dockerfile -t pub-sub . --no-cache
+docker build --file ./pub-sub/Dockerfile -t pub-sub . --no-cache
 docker tag pub-sub gcr.io/f1-telemetry-app/pub-sub
 docker push gcr.io/f1-telemetry-app/pub-sub
