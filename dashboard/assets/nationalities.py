6297f6ffdc9aa63f1bf2a379bd0106f3838b579a
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-05-30 20:29:18 +0200

    creating nationalities dataset for future tables use

---
+++ dashboard/assets/nationalities.py
@@ -0,0 +1,518 @@
+NATIONALITIES = {
+	'American': {
+		'id': 1,
+		'country': 'United States',
+		'country_code': 'US',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/US.svg'
+	},
+	'Argentinian': {
+		'id': 2,
+		'country': 'Argentina',
+		'country_code': 'AR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/AR.svg'
+	},
+	'Australian': {
+		'id': 3,
+		'country': 'Australia',
+		'country_code': 'AU',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/AU.svg'
+	},
+	'Austrian': {
+		'id': 4,
+		'country': 'Austria',
+		'country_code': 'AT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/AT.svg'
+	},
+	'Azerbaijani': {
+		'id': 5,
+		'country': 'Azerbaijan',
+		'country_code': 'AZ',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/AZ.svg'
+	},
+	'Bahraini': {
+		'id': 6,
+		'country': 'Bahrain',
+		'country_code': 'BH',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/BH.svg'
+	},
+	'Belgian': {
+		'id': 7,
+		'country': 'Belgium',
+		'country_code': 'BE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/BE.svg'
+	},
+	'Bolivian': {
+		'id': 8,
+		'country': 'Bolivia',
+		'country_code': 'BO',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/BO.svg'
+	},
+	'Brazilian': {
+		'id': 9,
+		'country': 'Brazil',
+		'country_code': 'BR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/BR.svg'
+	},
+	'British': {
+		'id': 10,
+		'country': 'United Kingdom',
+		'country_code': 'GB',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GB.svg'
+	},
+	'Bulgarian': {
+		'id': 11,
+		'country': 'Bulgaria',
+		'country_code': 'BG',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/BG.svg'
+	},
+	'Cameroonian': {
+		'id': 12,
+		'country': 'Cameroon',
+		'country_code': 'CM',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CM.svg'
+	},
+	'Canadian': {
+		'id': 13,
+		'country': 'Canada',
+		'country_code': 'CA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CA.svg'
+	},
+	'Chilean': {
+		'id': 14,
+		'country': 'Chile',
+		'country_code': 'CL',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CL.svg'
+	},
+	'Chinese': {
+		'id': 15,
+		'country': 'China',
+		'country_code': 'CN',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CN.svg'
+	},
+	'Colombian': {
+		'id': 16,
+		'country': 'Colombia',
+		'country_code': 'CO',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CO.svg'
+	},
+	'Costa Rican': {
+		'id': 17,
+		'country': 'Costa Rica',
+		'country_code': 'CR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CR.svg'
+	},
+	'Croatian': {
+		'id': 18,
+		'country': 'Croatia',
+		'country_code': 'HR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/HR.svg'
+	},
+	'Cypriot': {
+		'id': 19,
+		'country': 'Cyprus',
+		'country_code': 'CY',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CY.svg'
+	},
+	'Czech': {
+		'id': 20,
+		'country': 'Czechia',
+		'country_code': 'CZ',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CZ.svg'
+	},
+	'Danish': {
+		'id': 21,
+		'country': 'Denmark',
+		'country_code': 'DK',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/DK.svg'
+	},
+	'Dutch': {
+		'id': 22,
+		'country': 'Netherlands',
+		'country_code': 'NL',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/NL.svg'
+	},
+	'Ecuadorian': {
+		'id': 23,
+		'country': 'Ecuador',
+		'country_code': 'EC',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/EC.svg'
+	},
+	'English': {
+		'id': 24,
+		'country': 'England',
+		'country_code': 'GB-ENG',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GB-ENG.svg'
+	},
+	'Emirian': {
+		'id': 25,
+		'country': 'United Arab Emirates',
+		'country_code': 'AE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/AE.svg'
+	},
+	'Estonian': {
+		'id': 26,
+		'country': 'Estonia',
+		'country_code': 'EE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/EE.svg'
+	},
+	'Finnish': {
+		'id': 27,
+		'country': 'Finland',
+		'country_code': 'FI',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/FI.svg'
+	},
+	'French': {
+		'id': 28,
+		'country': 'France',
+		'country_code': 'FR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/FR.svg'
+	},
+	'German': {
+		'id': 29,
+		'country': 'Germany',
+		'country_code': 'DE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/DE.svg'
+	},
+	'Ghanaian': {
+		'id': 30,
+		'country': 'Ghana',
+		'country_code': 'GH',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GH.svg'
+	},
+	'Greek': {
+		'id': 31,
+		'country': 'Greece',
+		'country_code': 'GR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GR.svg'
+	},
+	'Guatemalan': {
+		'id': 32,
+		'country': 'Guatemala',
+		'country_code': 'GT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GT.svg'
+	},
+	'Honduran': {
+		'id': 33,
+		'country': 'Honduras',
+		'country_code': 'HN',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/HN.svg'
+	},
+	'Hong Konger': {
+		'id': 34,
+		'country': 'Hong Kong',
+		'country_code': 'HK',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/HK.svg'
+	},
+	'Hungarian': {
+		'id': 35,
+		'country': 'Hungary',
+		'country_code': 'HU',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/HU.svg'
+	},
+	'Icelander': {
+		'id': 36,
+		'country': 'Iceland',
+		'country_code': 'IS',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/IS.svg'
+	},
+	'Indian': {
+		'id': 37,
+		'country': 'India',
+		'country_code': 'IN',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/IN.svg'
+	},
+	'Indonesian': {
+		'id': 38,
+		'country': 'Indonesia',
+		'country_code': 'ID',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/ID.svg'
+	},
+	'Irish': {
+		'id': 39,
+		'country': 'Ireland',
+		'country_code': 'IE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/IE.svg'
+	},
+	'Israeli': {
+		'id': 40,
+		'country': 'Israel',
+		'country_code': 'IL',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/IL.svg'
+	},
+	'Italian': {
+		'id': 41,
+		'country': 'Italy',
+		'country_code': 'IT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/IT.svg'
+	},
+	'Jamaican': {
+		'id': 42,
+		'country': 'Jamaica',
+		'country_code': 'JM',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/JM.svg'
+	},
+	'Japanese': {
+		'id': 43,
+		'country': 'Japan',
+		'country_code': 'JP',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/JP.svg'
+	},
+	'Jordanian': {
+		'id': 44,
+		'country': 'Jordan',
+		'country_code': 'JO',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/JO.svg'
+	},
+	'Kuwaiti': {
+		'id': 45,
+		'country': 'Kuwait',
+		'country_code': 'KW',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/KW.svg'
+	},
+	'Latvian': {
+		'id': 46,
+		'country': 'Latvia',
+		'country_code': 'LV',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/LV.svg'
+	},
+	'Lebanese': {
+		'id': 47,
+		'country': 'Lebanon',
+		'country_code': 'LB',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/LB.svg'
+	},
+	'Lithuanian': {
+		'id': 48,
+		'country': 'Lithuania',
+		'country_code': 'LT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/LT.svg'
+	},
+	'Luxembourger': {
+		'id': 49,
+		'country': 'Luxembourg',
+		'country_code': 'LU',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/LU.svg'
+	},
+	'Malaysian': {
+		'id': 50,
+		'country': 'Malaysia',
+		'country_code': 'MY',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/MY.svg'
+	},
+	'Maltese': {
+		'id': 51,
+		'country': 'Malta',
+		'country_code': 'MT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/MT.svg'
+	},
+	'Mexican': {
+		'id': 52,
+		'country': 'Mexico',
+		'country_code': 'MX',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/MX.svg'
+	},
+	'Monegasque': {
+		'id': 53,
+		'country': 'Monaco',
+		'country_code': 'MC',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/MC.svg'
+	},
+	'New Zealander': {
+		'id': 54,
+		'country': 'New Zealand',
+		'country_code': 'NZ',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/NZ.svg'
+	},
+	'Nicaraguan': {
+		'id': 55,
+		'country': 'Nicaragua',
+		'country_code': 'NI',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/NI.svg'
+	},
+	'North Korean': {
+		'id': 56,
+		'country': 'North Korea',
+		'country_code': 'KP',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/KP.svg'
+	},
+	'Northern Irish': {
+		'id': 57,
+		'country': 'Northern Ireland',
+		'country_code': 'GB-NIR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GB-NIR.svg'
+	},
+	'Norwegian': {
+		'id': 58,
+		'country': 'Norway',
+		'country_code': 'NO',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/NO.svg'
+	},
+	'Omani': {
+		'id': 59,
+		'country': 'Oman',
+		'country_code': 'OM',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/OM.svg'
+	},
+	'Pakistani': {
+		'id': 60,
+		'country': 'Pakistan',
+		'country_code': 'PK',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PK.svg'
+	},
+	'Panamanian': {
+		'id': 61,
+		'country': 'Panama',
+		'country_code': 'PA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PA.svg'
+	},
+	'Paraguayan': {
+		'id': 62,
+		'country': 'Paraguay',
+		'country_code': 'PY',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PY.svg'
+	},
+	'Peruvian': {
+		'id': 63,
+		'country': 'Peru',
+		'country_code': 'PE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PE.svg'
+	},
+	'Polish': {
+		'id': 64,
+		'country': 'Poland',
+		'country_code': 'PL',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PL.svg'
+	},
+	'Portuguese': {
+		'id': 65,
+		'country': 'Portugal',
+		'country_code': 'PT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/PT.svg'
+	},
+	'Qatari': {
+		'id': 66,
+		'country': 'Qatar',
+		'country_code': 'QA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/QA.svg'
+	},
+	'Romanian': {
+		'id': 67,
+		'country': 'Romania',
+		'country_code': 'RO',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/RO.svg'
+	},
+	'Russian': {
+		'id': 68,
+		'country': 'Russia',
+		'country_code': 'RU',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/RU.svg'
+	},
+	'Salvadoran': {
+		'id': 69,
+		'country': 'El Salvador',
+		'country_code': 'SV',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SV.svg'
+	},
+	'Saudi': {
+		'id': 70,
+		'country': 'Saudi Arabia',
+		'country_code': 'SA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SA.svg'
+	},
+	'Scottish': {
+		'id': 71,
+		'country': 'Scotland',
+		'country_code': 'GB-SCT',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GB-SCT.svg'
+	},
+	'Serbian': {
+		'id': 72,
+		'country': 'Serbia',
+		'country_code': 'RS',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/RS.svg'
+	},
+	'Singaporean': {
+		'id': 73,
+		'country': 'Singapore',
+		'country_code': 'SG',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SG.svg'
+	},
+	'Slovakian': {
+		'id': 74,
+		'country': 'Slovakia',
+		'country_code': 'SK',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SK.svg'
+	},
+	'Slovenian': {
+		'id': 75,
+		'country': 'Slovenia',
+		'country_code': 'SI',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SI.svg'
+	},
+	'South Korean': {
+		'id': 76,
+		'country': 'South Korea',
+		'country_code': 'KR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/KR.svg'
+	},
+	'South African': {
+		'id': 77,
+		'country': 'South Africa',
+		'country_code': 'ZA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/ZA.svg'
+	},
+	'Spanish': {
+		'id': 78,
+		'country': 'Spain',
+		'country_code': 'ES',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/ES.svg'
+	},
+	'Swedish': {
+		'id': 79,
+		'country': 'Sweden',
+		'country_code': 'SE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/SE.svg'
+	},
+	'Swiss': {
+		'id': 80,
+		'country': 'Switzerland',
+		'country_code': 'CH',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/CH.svg'
+	},
+	'Thai': {
+		'id': 81,
+		'country': 'Thailand',
+		'country_code': 'TH',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/TH.svg'
+	},
+	'Turkish': {
+		'id': 82,
+		'country': 'Turkey',
+		'country_code': 'TR',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/TR.svg'
+	},
+	'Uruguayan': {
+		'id': 83,
+		'country': 'Uruguay',
+		'country_code': 'UY',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/UY.svg'
+	},
+	'Ukrainian': {
+		'id': 84,
+		'country': 'Ukraine',
+		'country_code': 'UA',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/UA.svg'
+	},
+	'Venezuelan': {
+		'id': 85,
+		'country': 'Venezuela',
+		'country_code': 'VE',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/VE.svg'
+	},
+	'Welsh': {
+		'id': 86,
+		'country': 'Wales',
+		'country_code': 'GB-WLS',
+		'flag_url': 'https://raw.githubusercontent.com/yammadev/flag-icons/master/svg/GB-WLS.svg'
+	}
+}
