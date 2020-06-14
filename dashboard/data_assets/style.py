56a52959f61a9004117368484cb67c187d28facb
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-06-14 23:17:44 +0200

    update and revamp structure in supplementary files

---
+++ dashboard/data_assets/style.py
@@ -0,0 +1,232 @@
+SIDEBAR_STYLE = {
+    "width": "256px",
+    'min-height': '100vh',
+    "padding": "32px 16px",
+    "background-color": "#1A1A24",
+    'float': 'left'
+}
+
+CONTENT_STYLE = {
+    "margin-left": "400px",
+    "margin-right": "32px",
+    "padding": "32px 16px",
+    'float': 'left',
+    'background-color': '#111118;'
+}
+
+DEFAULT_BACKGROUND = {
+    'background-color': '#111118;'
+}
+
+SECTION_HEADINGS = {
+    'padding-top': '32px',
+    'text-align': 'left',
+    'padding-left': '510px'
+}
+
+ENABLED_LINK_TEXT_STYLE = {
+    'margin-top': '10px', 
+    'color': '#FF201E'
+}
+
+DISABLED_LINK_TEXT_STYLE = {
+    'margin-top': '10px', 
+    'color': '#f8f9fa'
+}
+
+ENABLED_LINK_TEXT_STYLE_BOLD = {
+    'margin-top': '10px', 
+    'color': '#FFFFFF',
+    'font-size': '60px',
+    'font-style': 'bold'
+}
+
+ENABLED_TILE_STYLE = {
+    'height': '200px',
+    'width': '200px',
+    'float': 'left',
+    'text-align': 'center',
+    'padding': '25px',
+    'border-radius': '30px',
+    'background-color': '#FFA4A3'
+}
+
+DISABLED_TILE_STYLE = {
+    'height': '200px',
+    'width': '200px',
+    'float': 'left',
+    'text-align': 'center',
+    'padding': '25px',
+    'border-radius': '30px',
+    "pointer-events": "none", 
+    "cursor": "default", 
+    'background-color': '#9D9D9E'
+}
+
+TILE_WRAPPER = {
+    'height': '250px',
+    'display':'inline-block'
+}
+
+CENTERED = {
+    'text-align': 'center'
+}
+
+MENU_ROW_SIZE = {
+    'height', '40px'
+}
+
+WHITE_COLOR = {
+    'filter': 'invert(100%) sepia(100%) saturate(1%) hue-rotate(291deg) brightness(104%) contrast(101%)'
+}
+
+BLACK_COLOR = {
+    'filter': 'invert(0%) sepia(0%) saturate(10%) hue-rotate(308deg) brightness(101%) contrast(100%)'
+}
+
+GITLAB_COLOR = {
+    'filter': 'invert(52%) sepia(37%) saturate(2792%) hue-rotate(342deg) brightness(98%) contrast(101%)'
+}
+
+DISABLED_COLOR = {
+    'filter': 'invert(100%) sepia(14%) saturate(129%) hue-rotate(141deg) brightness(100%) contrast(96%)'
+}
+
+DISABLED_COLOR2 = {
+    'filter': 'invert(67%) sepia(7%) saturate(39%) hue-rotate(202deg) brightness(93%) contrast(88%)'
+}
+
+ENABLED_COLOR = {
+    'filter': 'invert(34%) sepia(90%) saturate(6677%) hue-rotate(351deg) brightness(111%) contrast(101%)'
+}
+
+GREEN_COLOR = {
+    'filter': 'invert(23%) sepia(68%) saturate(1198%) hue-rotate(70deg) brightness(99%) contrast(103%)'
+}
+
+ROW_MARGIN_SMALL = {
+    'margin': '0px 5px'
+}
+
+ROW_MARGIN_BIG = {
+    'margin': '0px 20px'
+}
+
+TEAMS_COLORS = {
+    'Mercedes': {
+            'hex': '#00D2BE',
+            'filter': 'invert(58%) sepia(53%) saturate(1151%) hue-rotate(129deg) brightness(97%) contrast(101%)'
+        },
+    'Ferrari': {
+        'hex': '#C00000',
+        'filter': 'invert(9%) sepia(67%) saturate(7498%) hue-rotate(1deg) brightness(102%) contrast(113%)'
+    },
+    'Red Bull Racing': {
+        'hex': '#0600EF',
+        'filter': 'invert(9%) sepia(100%) saturate(6898%) hue-rotate(250deg) brightness(81%) contrast(139%)'
+    },
+    'Renault': {
+        'hex': '#FFF500',
+        'filter': 'invert(74%) sepia(93%) saturate(1095%) hue-rotate(7deg) brightness(113%) contrast(105%)'
+    },
+    'Haas': {
+        'hex': '#787878',
+        'filter': 'invert(47%) sepia(0%) saturate(0%) hue-rotate(260deg) brightness(99%) contrast(93%)'
+    },
+    'Racing Point': {
+        'hex': '#F596C8',
+        'filter': 'invert(69%) sepia(62%) saturate(540%) hue-rotate(291deg) brightness(106%) contrast(92%)'
+    },
+    'Alpha Tauri': {
+        'hex': '#C8C8C8',
+        'filter': 'invert(88%) sepia(0%) saturate(450%) hue-rotate(191deg) brightness(94%) contrast(88%)'
+    },
+    'McLaren': {
+        'hex': '#FF8700',
+        'filter': 'invert(53%) sepia(48%) saturate(1858%) hue-rotate(360deg) brightness(101%) contrast(107%)'
+    },
+    'Alfa Romeo': {
+        'hex': '#960000',
+        'filter': 'invert(13%) sepia(33%) saturate(6885%) hue-rotate(350deg) brightness(103%) contrast(122%)'
+    },
+    'Williams': {
+        'hex': '#0082FA',
+        'filter': 'invert(48%) sepia(78%) saturate(5193%) hue-rotate(192deg) brightness(98%) contrast(101%)'
+    },
+    'Toro Rosso': {
+            'hex': '#469BFF',
+            'filter': 'invert(45%) sepia(86%) saturate(789%) hue-rotate(190deg) brightness(105%) contrast(101%)'
+    },
+    'Trident': {
+            'hex': '#4166F3',
+            'filter': 'filter: invert(29%) sepia(79%) saturate(2689%) hue-rotate(222deg) brightness(101%) contrast(91%)'
+    },
+    'BWT Arden': {
+            'hex': '#eea5d0',
+            'filter': 'invert(73%) sepia(32%) saturate(445%) hue-rotate(281deg) brightness(96%) contrast(94%)'
+    },
+    'MP Motorsport': {
+            'hex': '#f6817a',
+            'filter': 'invert(89%) sepia(34%) saturate(6928%) hue-rotate(310deg) brightness(98%) contrast(96%)'
+    },
+    'Campos Vexatec Racing': {
+            'hex': '#f56a19',
+            'filter': 'invert(57%) sepia(92%) saturate(3954%) hue-rotate(353deg) brightness(101%) contrast(92%)'
+    },
+    'Charouz Racing System': {
+            'hex': '#a3103d',
+            'filter': 'invert(10%) sepia(62%) saturate(6113%) hue-rotate(333deg) brightness(95%) contrast(94%)'
+    },
+    'Pertamina': {
+            'hex': '#f45d52',
+            'filter': 'invert(60%) sepia(51%) saturate(5312%) hue-rotate(328deg) brightness(98%) contrast(96%)'
+    },
+    'Virtuosi Racing': {
+            'hex': '#f9d71d',
+            'filter': 'invert(91%) sepia(50%) saturate(7480%) hue-rotate(333deg) brightness(96%) contrast(104%)'
+    },
+    'DAMS': {
+            'hex': '#56ddf0',
+            'filter': 'invert(88%) sepia(34%) saturate(1809%) hue-rotate(155deg) brightness(97%) contrast(93%)'
+    },
+    'ART Grand Prix': {
+            'hex': '#58050f',
+            'filter': 'invert(10%) sepia(30%) saturate(7050%) hue-rotate(338deg) brightness(86%) contrast(107%)'
+    },
+    'Carlin': {
+            'hex': '#1c6258',
+            'filter': 'invert(32%) sepia(12%) saturate(2066%) hue-rotate(122deg) brightness(94%) contrast(91%)'
+    },
+    'Russian Time': {
+            'hex': '#1040ac',
+            'filter': 'invert(13%) sepia(92%) saturate(3048%) hue-rotate(221deg) brightness(104%) contrast(94%)'
+    },
+    'Hitech Grand Prix': {
+            'hex': '#ffffff',
+            'filter': 'invert(100%) sepia(0%) saturate(0%) hue-rotate(10deg) brightness(102%) contrast(106%)'
+    },
+    'Red Bull 2010': {
+            'hex': '#d9d9d9',
+            'filter': 'invert(96%) sepia(0%) saturate(0%) hue-rotate(158deg) brightness(91%) contrast(93%)'
+    },
+    'Brawn 2009': {
+            'hex': '#d9d9d9',
+            'filter': 'invert(96%) sepia(0%) saturate(0%) hue-rotate(158deg) brightness(91%) contrast(93%)'
+    },
+    'McLaren 2010': {
+            'hex': '#d9d9d9',
+            'filter': 'invert(96%) sepia(0%) saturate(0%) hue-rotate(158deg) brightness(91%) contrast(93%)'
+    },
+    'Ferrari 2004': {
+            'hex': '#d9d9d9',
+            'filter': 'invert(96%) sepia(0%) saturate(0%) hue-rotate(158deg) brightness(91%) contrast(93%)'
+    },
+    'Williams 2003': {
+            'hex': '#d9d9d9',
+            'filter': 'invert(96%) sepia(0%) saturate(0%) hue-rotate(158deg) brightness(91%) contrast(93%)'
+    },
+    'Unknown': {
+            'hex': '#565656',
+            'filter': 'invert(32%) sepia(0%) saturate(39%) hue-rotate(199deg) brightness(98%) contrast(86%)'
+    }
+}
