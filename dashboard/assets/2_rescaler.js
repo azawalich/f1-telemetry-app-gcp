37ee1b31f21a39c4cf1a1786201eb4725f02ebea
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-06-22 15:52:30 +0200

    add jQuery rescaler

---
+++ dashboard/assets/2_rescaler.js
@@ -0,0 +1,45 @@
+function resize_dashboard(layout_width, window_width) {
+    if (window_width > 1750){
+    $("#scaleable-wrapper").css({
+            transform: "scale(" + 0.8*(window_width / layout_width) + ")",  
+            'transform-origin': 'top left'
+        });
+    $("#sidebar").css({
+        'height': $(window).height() * 0.9
+    });
+  } else {
+    $("#scaleable-wrapper").attr('style', '');
+    $("#layout-design").attr('style', '');
+  }
+};
+
+var layout_width = $("#scaleable-wrapper").width();
+var window_width = $(window).width();
+
+$(window).on('load resize', function() {
+    var dash_state = $('_dash-loading-callback')['prevObject'][0]['readyState']
+
+    if (dash_state == 'complete'){
+        setTimeout(function() {   
+            var layout_width = $("#scaleable-wrapper").width();
+            var window_width = $(window).width();
+            resize_dashboard(layout_width, window_width);
+        }, 100);
+    }
+    resize_dashboard(layout_width, window_width);
+}).trigger('resize');
+
+$(window).scroll(function() {
+    // hacky solution to make scrolling work
+    // $("#sidebar").css({
+    //     "top": $(window).scrollTop(),
+    // });
+
+    // $("#header-top-row").css({
+    //     "top": $(window).scrollTop()
+    // });
+
+    // $('#scroll-background').css({
+    //     "top": $(window).scrollTop()
+    // });
+})
