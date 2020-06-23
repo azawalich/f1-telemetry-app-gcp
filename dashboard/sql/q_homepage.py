2df4ce621c0a25833993bd060e1d954e6be96c68
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-06-23 22:54:26 +0200

    query fix for changing weather session duplicates

--- dashboard/sql/q_homepage.py
+++ dashboard/sql/q_homepage.py
@@ -3,6 +3,7 @@ select * from `f1-telemetry-app.dashboard_data.statistics` as dd_stats
 JOIN (select id as team_id, value as team from `f1-telemetry-app.definitions.team`) as def_team using(team_id)
 JOIN (select id as nationality_id, value as nationality from `f1-telemetry-app.definitions.nationality`) as def_nats using(nationality_id)
 JOIN (select id as track_id, value as track from `f1-telemetry-app.definitions.track`) as def_tck using(track_id)
-JOIN (select distinct sessionUID, weather, airTemperature, trackTemperature, trackLength from `f1-telemetry-app.dashboard_data.session`, unnest(header) as header) as def_ses using(sessionUID)
+JOIN (select distinct sessionUID, weather, trackLength from `f1-telemetry-app.dashboard_data.session`, unnest(header) as header) as def_ses using(sessionUID)
+JOIN (select sessionUID, max(airTemperature) as airTemperature, max(trackTemperature) as trackTemperature from `f1-telemetry-app.dashboard_data.session`, unnest(header) as header group by sessionUID) as def_ses2 using(sessionUID)
 order by publish_time desc
 '''
