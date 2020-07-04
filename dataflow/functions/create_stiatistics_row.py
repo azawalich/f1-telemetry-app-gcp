d10c478bfcb0269dd23795a2b5d286c98be49a41
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-07-04 17:30:35 +0200

    implement row checking also in statistics_row

--- dataflow/functions/create_stiatistics_row.py
+++ dataflow/functions/create_stiatistics_row.py
@@ -26,23 +26,13 @@ def create_stiatistics_row(statistics_dict, packet_id, rows_to_add_reduced):
         statistics_dict['trackLength'] = last_row['trackLength']
     elif packet_id == 2: # lap table
         player_indeks = last_row['header'][0]['playerCarIndex']
-        full_laps = []
-        for single_row_indeks in range(0, len(rows_to_add_reduced)):
-            single_row = rows_to_add_reduced[single_row_indeks]
-            if single_row['lapData'][player_indeks]['sector'] == 2:
-                full_laps.append(
-                    '{}_{}'.format(
-                        single_row['lapData'][player_indeks]['sector'],
-                        single_row['lapData'][player_indeks]['currentLapNum']
-                    )
-                )
-        statistics_dict['fullLaps'] = len(set(full_laps))
-        statistics_dict['lap_count'] = statistics_dict['fullLaps']
         # to get fastest lap we need to iterate through all lap rows
+        full_laps = []
         lap_times = []
         for single_row_indeks in range(0, len(rows_to_add_reduced)):
             single_row = rows_to_add_reduced[single_row_indeks]
             lap_times.append(single_row['lapData'][player_indeks]['lastLapTime'])
+            full_laps.append(single_row['lapData'][player_indeks])
         
         statistics_dict['fastest_lap'] = min(i for i in lap_times if i > 0) 
         statistics_dict['fastest_lap_format'] = str(
@@ -59,6 +49,19 @@ def create_stiatistics_row(statistics_dict, packet_id, rows_to_add_reduced):
         statistics_dict['record_lap_format'] = str(
             datetime.timedelta(seconds=statistics_dict['record_lap'])
             )[2:-3]
+        
+        full_laps_sectors = []
+        for single_lap_indeks in range(0, len(full_laps)):
+            single_row = full_laps[single_lap_indeks]
+            if single_row['sector'] == 2 and \
+                single_row['sector1Time'] > 0 and \
+                    single_row['sector2Time'] > 0 and \
+                        single_row['pitStatus'] == 0:
+                full_laps_sectors.append(
+                    single_row
+                )
+        statistics_dict['fullLaps'] = len(full_laps_sectors)
+        statistics_dict['lap_count'] = statistics_dict['fullLaps']
     elif packet_id == 3: # event table
         player_indeks = last_row['header'][0]['playerCarIndex']
 
