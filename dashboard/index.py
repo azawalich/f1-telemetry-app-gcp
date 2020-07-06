fecc8d7a9386fa0b5e4c139f77a4af9a2c82e406
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2020-07-06 20:44:34 +0200

    force mobile users to move their phones

--- dashboard/index.py
+++ dashboard/index.py
@@ -19,23 +19,49 @@ header = html.Div(
 
 def serve_layout():
     layout = html.Div(
-        html.Div(
         [
-            dcc.Location(id="url"),
-            sidebar,
-            header,
-            content,
-            html.Div(0, id='paging-state-default', style={'display': 'none'}),
-            html.Div('All Sessions', id='choice-default', style={'display': 'none'}),
-            html.Div(id='paging-state', style={'display': 'none'}),
-            html.Div(id='choice', style={'display': 'none'})
-        ],
-        id = 'layout-design',
-        className = 'layout-design'
-        ),
-        id = 'scaleable-wrapper',
-        className = 'scaleable-wrapper'
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            html.Img(src="https://upload.wikimedia.org/wikipedia/commons/3/33/F1.svg", id='mobile-logo'),
+                            html.Img(src="./assets/images/TELEMETRY.svg", id='mobile-logo2'),
+                        ]
+                    ),
+                    html.Div(
+                        [   
+                            html.H1('Rotate phone to landscape for getting full F1 experience ;).'),
+                        ]
+                    ),
+                    html.Div(
+                        [   
+                            html.Img(src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Repeat_font_awesome.svg", id='mobile-refresh')
+                        ]
+                    )
+                ],
+            id='rotate-phone'
+            ),
+            html.Div(
+                html.Div(
+                [
+                    dcc.Location(id="url"),
+                    sidebar,
+                    header,
+                    content,
+                    html.Div(0, id='paging-state-default', style={'display': 'none'}),
+                    html.Div('All Sessions', id='choice-default', style={'display': 'none'}),
+                    html.Div(id='paging-state', style={'display': 'none'}),
+                    html.Div(id='choice', style={'display': 'none'})
+                ],
+                id = 'layout-design',
+                className = 'layout-design'
+                ),
+                id = 'scaleable-wrapper',
+                className = 'scaleable-wrapper'
+            )
+        ]
     )
+    
     
     return layout
 
